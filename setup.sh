568f97385148498400ea6d8a517135d03196aa9a
smurph <56486787+mySmurph@users.noreply.github.com>
2022-01-12 11:49:43 -0800

    no actions

--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
 sudo yum update
+y
 
 sudo rpm -Uvh https://packages.microsoft.com/config/centos/7/packages-microsoft-prod.rpm
 sudo yum install dotnet-sdk-5.0
+
+y
+
+y
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install v14.18.1
 
 npm install -g @angular/cli
+
+n
+
+ng version
