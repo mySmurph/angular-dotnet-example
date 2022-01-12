1c7755123f84301afb22ea27b14064df55da4699
smurph <56486787+mySmurph@users.noreply.github.com>
2022-01-12 11:02:07 -0800

    install base dependencies

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
-sudo amazon-linux-extras install docker -y
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo chmod 666 /var/run/docker.sock
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-sudo service docker start
+sudo yum update
+
+sudo rpm -Uvh https://packages.microsoft.com/config/centos/7/packages-microsoft-prod.rpm
+sudo yum install dotnet-sdk-5.0
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install v14.18.1
+
+npm install -g @angular/cli
